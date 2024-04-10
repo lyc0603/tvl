8e4e4b97eeafe0b61d532e693db6c9d971446fe1
lyc0603 <luoyichencn110110@gmail.com>
2024-04-10 15:07:06 +0000

    update

--- scripts/visualization/risk_analysis.py
+++ scripts/visualization/risk_analysis.py
@@ -129,7 +129,7 @@ for event, date in SAMPLE_DATA_DICT.items():
             plt.FuncFormatter(lambda x, loc: "{:.1f}B".format(x / 1e9))
         )
         plt.subplots_adjust(left=0.25)
-
+        plt.tight_layout()
         plt.savefig(
             f"{FIGURE_PATH}/sensitivity_{event}_{PARAMS_NAMEING_MAPPING[params]}.pdf",
             dpi=2000,
