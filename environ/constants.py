528249458c248a13d9009d1714c5b7aecc236e8e
lyc0603 <luoyichencn110110@gmail.com>
2024-05-30 19:27:13 +0000

    update

--- environ/constants.py
+++ environ/constants.py
@@ -102,10 +102,10 @@ LLAMA_STABLE = "https://stablecoins.llama.fi/stablecoins?includePrices=true"
 
 # sample date
 SAMPLE_DATA_DICT = {
-    # "max_tvl": "2021-12-26",
-    # "luna_collapse": "2022-05-09",
-    # "ftx_collapse": "2022-11-08",
-    "current_date": "2024-03-01",
+    "max_tvl": "2021-12-26",
+    "luna_collapse": "2022-05-09",
+    "ftx_collapse": "2022-11-08",
+    # "current_date": "2024-03-01",
 }
 
 # Coingecko API KEY
@@ -214,6 +214,6 @@ params_dict = {
     "$\\psi_{1,AAVE}$": {
         "$\\delta_{MKR}$": [1, 1, 1],
         "$\\delta_{AAVE}$": [0.5, 0.5, 0.5],
-        "$\\psi_{1,AAVE}$": [0, 0.5, 0.95],
+        "$\\psi_{1,AAVE}$": [0, 0.90, 0.95],
     },
 }
